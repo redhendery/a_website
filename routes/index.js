d68eea4db3ca57532dc62b3680e9ce3b34a551a4
redhendery <redhendery@gmail.com>
2016-12-23 11:56:42 -0800

    add about & contact page with routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'A Website!' });
 });
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact')
+})
+
+router.get('/about', (req, res, next) => {
+  res.render('about')
+})
 
 module.exports = router;
